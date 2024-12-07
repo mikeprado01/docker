8d61f2217fbbb095f44c6d9a6f8f06a741989c0d
Mike Prado <michael.prado@globe.com.ph>
2024-12-07 23:03:34 +0800

    Modified and run init-products-db.sh scipt

--- hackathon/src/db/postgres/init-products-db.sh
+++ hackathon/src/db/postgres/init-products-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
 
     CREATE TABLE public.products (
         id bigint NOT NULL,
